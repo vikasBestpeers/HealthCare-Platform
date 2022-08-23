c895331f1845486a63094a73ce1f2730fc36f049
vikasBestpeers <vsisodiya@bestpeers.com>
2022-08-23 11:34:53 +0530

    validation

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,8 @@
 class Doctor < ApplicationRecord
     belongs_to :user
+    validates :name, presence: true
+    validates :surname, presence: true
+    validates :specialization, presence: true
+    validates :experience, presence: true
+
 end
